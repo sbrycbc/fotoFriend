cb12872a1c12bfb66d966c38fd8d39f89f8e26cf
sbrycbc <sabriyecbc@gmail.com>
2023-09-26 18:34:31 +0200

    added public page and photoController and -Route..updated style...

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import conn from './db.js';
 import pageRoute from './routes/pageRoute.js';
+import photoRoute from './routes/photoRoute.js';
 
 
 dotenv.config();
@@ -19,9 +20,11 @@ app.set('view engine', 'ejs');
 
 //static files middleware
 app.use(express.static('public'));
+app.use(express.json());
 
 //router
 app.use("/", pageRoute)
+app.use("/photos", photoRoute)
 
 /* app.get('/', (req, res) => {
     res.render('index');
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,10 +1,16 @@
 const getIndexPage = (req, res) => {
-    res.render("index")
+    res.render("index",{
+        link:'index',
+    })
 }
     
 const getAboutPage = (req, res) => {
-        res.render("about")
+    res.render('about', {
+        link: 'about',
+      });
 }
+
+
     
     
     
