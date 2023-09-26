1dcf04b663c3ffbe8f19fea8e1390331c4fd41de
sbrycbc <sabriyecbc@gmail.com>
2023-09-26 13:43:11 +0200

    added views and partials ejs..

---
+++ db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const conn = () => {
+    mongoose.connect(process.env.DB_URI, {
+       dbName: "foto_friend",
+       useNewUrlParser: true,
+       useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connected to the DB succesully');
+      })
+    .catch((err) => {
+        console.log(`DB connection err:, ${err}`);
+      });
+};
+
+export default conn;
