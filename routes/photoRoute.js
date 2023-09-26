a94f878f903bc6e440a6fb5690b91754a54cec45
sbrycbc <sabriyecbc@gmail.com>
2023-09-26 23:03:31 +0200

    added update photo router, function and controller..

--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -12,6 +12,7 @@ router
 
 router.route("/:id").get(photoController.getAPhoto);
 router.route("/:id").delete(photoController.deletePhoto);
+router.route("/:id").put(photoController.updatePhoto);
 
 
 export default router;
