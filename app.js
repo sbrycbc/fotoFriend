2a3aa31096f5fd900072769b76d662a768e60c33
sbrycbc <sabriyecbc@gmail.com>
2023-09-26 13:55:04 +0200

    added pageController and -Route...

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import conn from './db.js';
+import pageRoute from './routes/pageRoute.js';
 
 
 dotenv.config();
@@ -19,14 +20,16 @@ app.set('view engine', 'ejs');
 //static files middleware
 app.use(express.static('public'));
 
+//router
+app.use("/", pageRoute)
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.render('index');
 });
 
 app.get('/about', (req, res) => {
     res.render('about');
-});
+}); */
 
 
 
